7557dbfbd180c25acc01b475cccf67f9fd831ee2
Matt Girard <mgirard@umassd.edu>
2018-02-06 15:12:45 -0500

    Adding account functions

---
+++ {{cookiecutter.repo_name}}/src/vis/example-shiny-app/account_functions.R
@@ -0,0 +1,48 @@
+library(digest)
+
+proj_root_dir <- Sys.getenv("PROJECT_ROOT")
+raw_data_dir <- paste0(proj_root_dir, "/data/raw/")
+data_dir <- paste0(proj_root_dir, "/data/")
+sql_dir <- paste0(proj_root_dir, "/src/data/sql/")
+
+#Add user with username (user) and password (pw), prevent duplicate users
+#Password is stored securely as an MD5 hash
+addUser <- function(user, pw){
+  source(paste0(sql_dir, "vertica_connect.R"))
+  query <- paste0("SELECT username FROM ", Sys.getenv("credentials_location"))
+  users <- dbGetQuery(db, query)
+  if(user %in% users$username){
+    dbDisconnect(db)
+    stop(paste0("User ", user, " already exists. Select a different username."))
+  }
+  else{
+    pw <- digest(pw, algo = "md5")
+    query <- paste0("INSERT INTO ", Sys.getenv("credentials_location"), " VALUES (?,?);")
+    dbSendUpdate(db, query, user, pw)
+    dbDisconnect(db)
+  }
+
+}
+
+#Get a list of users and their hashed passwords
+getUsers <- function(){
+  source(paste0(sql_dir, "vertica_connect.R"))
+  query <- paste0("SELECT * FROM ", Sys.getenv("credentials_location"), ";")
+  users <- dbGetQuery(db, query)
+  dbDisconnect(db)
+  return(users)
+}
+
+#Remove a user with username user
+removeUser <- function(user){
+  source(paste0(sql_dir, "vertica_connect.R"))
+  query <- paste0(
+    "DELETE FROM ", Sys.getenv("credentials_location"), " WHERE username='",
+    user,
+    "';"
+  )
+  dbSendUpdate(db, query)
+  dbDisconnect(db)
+}
+
+
