727f077836fa8888da67dd901e83ff8074131c58
Matt Girard <mgirard@umassd.edu>
2018-02-20 08:52:31 -0500

    Updating port number constraints

--- {{cookiecutter.repo_name}}/setup_container.sh
+++ {{cookiecutter.repo_name}}/setup_container.sh
@@ -2,13 +2,14 @@
 
 if [ $# -eq 0 ] || [ -z "$1" ]
     then
-  echo "You must supply a port number"
-  exit 1
+      echo "You must supply a port number"
+      exit 1
 fi
 
-if [ $1 < 51000 ] || [$1 > 52000]
+if [ $1 -lt 51000 ] || [$1 -gt 52000]
   then
     echo "Please supply a port number in the range 51000-52000"
+    exit 1
 fi
 
 container_name=$(basename $(git remote get-url origin) .git)
