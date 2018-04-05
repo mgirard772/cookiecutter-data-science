2f26cd6a0f6079a236395cde9479770d10481f16
Matt Girard <mgirard@umassd.edu>
2018-04-05 15:09:47 -0400

    Adding s3 sync functionality

---
+++ {{cookiecutter.repo_name}}/setup_s3.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+#Source your vertica credentials
+source .Renviron
+
+#Generate vertica.ini from environment variables
+cat << EOM > vertica.ini
+[DEFAULT]
+host=$vertica_host
+port=$vertica_port
+database=advana
+user=$premium_finance_batch
+password=$premium_finance_pass
+EOM
+
+#Get aws s3 credentials from Vertica
+#Install this tool with the following command: 
+#pip install git+ssh://git@github.com/massmutual/set-aws-credentials
+set-aws-credentials vertica.ini data-scientist
+
+#Source aws-credentials then remove them and vertica.ini
+source ./aws-credentials.sh
+rm aws-credentials.sh vertica.ini
